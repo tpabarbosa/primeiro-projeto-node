23c3248e72480e0a96c45304bdf704eb72b49237
Tatiana Barbosa <tpabarbosa@yahoo.com.br>
2020-11-03 23:22:44 -0300

    desafio primeiro-projeto-node

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,14 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: RequestDTO): Transaction {
+    if (type === 'outcome' && (this.transactionsRepository.getBalance().total - value) < 0) {
+       throw Error('error string');
+    }
+
+    const transaction = this.transactionsRepository.create( { title, value, type } )
+
+    return transaction;
   }
 }
 
